b33f3b600f14ffa2c9f57784e9ba1b35e1e2dc38
Ramon Llenado <rmllenado@yahoo.com>
2022-03-07 13:57:56 -0500

    changes root controller

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Azure Spring Cloud!";
+        return "Greetings from Azure Spring Cloud! THIS IS THE GREEN DEPLOYMENT";
     }
 }
